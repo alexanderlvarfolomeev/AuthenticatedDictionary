7bca954c0d7ed8b9502868edd16276681549334e
Varfolomeev Alexander <vas.aleks.v@gmail.com>
2020-10-24 02:26:02 +0300

    MLP was added

--- benc_test.py
+++ benc_test.py
@@ -3,6 +3,7 @@ import unittest
 import skiplist
 import time
 import math
+import matplotlib.pyplot as mlp
 
 
 def int2bytes(item: int) -> bytes:
@@ -10,26 +11,43 @@ def int2bytes(item: int) -> bytes:
 
 
 class MyTestCase(unittest.TestCase):
+    benches = [[], [], []]
 
     def test_set_time(self):
         list = skiplist.SkipList()
         for i in range(1, 1000001):
             list.update(True, int2bytes(i))
             if i % 100000 == 0:
-                ints = [int2bytes(random.randint(1, i)) for _ in range(100)]
+                print("<--- %s elements --->" % i)
+                ints = [int2bytes(random.randint(1, i)) for _ in range(1000)]
                 start_time = time.time()
                 for j in ints:
                     list.update(True, j)
-                print("<--- %s elements --->" % i)
-                print("--- 1 insert operation %s seconds ---" % ((time.time() - start_time) / 100))
+                t = (time.time() - start_time)
+                self.benches[0].append(t)
+                print("--- 1 insert operation %s ms ---" % t)
                 start_time = time.time()
                 for j in ints:
                     list.update(False, j)
-                print("--- 1 deletion operation %s seconds ---" % ((time.time() - start_time) / 100))
+                t = (time.time() - start_time)
+                self.benches[1].append(t)
+                print("--- 1 deletion operation %s ms ---" % t)
                 start_time = time.time()
-                for _ in range(100):
+                for _ in range(1000):
                     list.verify(int2bytes(random.randint(1, i)))
-                print("--- 1 validation operation %s seconds ---" % ((time.time() - start_time) / 100))
+                t = (time.time() - start_time)
+                self.benches[2].append(t)
+                print("--- 1 validation operation %s ms ---" % t)
+        xs = [i * 100000 for i in range(1, 11)]
+        fig = mlp.figure(figsize=(16, 9))
+        mlp.scatter(xs, self.benches[0], s=6 ** 2, marker='^', label='insertion')
+        mlp.scatter(xs, self.benches[1], s=6 ** 2, marker='o', label='deletion')
+        mlp.scatter(xs, self.benches[2], s=6 ** 2, marker='s', label='query')
+        mlp.xlabel('number of elements', fontsize=14, fontweight='bold')
+        mlp.ylabel('average time (ms)', fontsize=14, fontweight='bold')
+        mlp.legend(fontsize=14)
+        mlp.savefig('result.png')
+        fig.show()
 
 
 def main():
